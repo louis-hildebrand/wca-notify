00c70e93796ac4066efa1a45b9332ba0e9eec5f8
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-10-15 14:30:32 -0400

    Skip previously-seen competitions

--- check_comps.py
+++ check_comps.py
@@ -4,15 +4,15 @@ file if any were found.
 """
 
 from dataclasses import dataclass
-from datetime import datetime, timedelta
+from datetime import datetime
 from inspect import cleandoc
 from pathlib import Path
 
 import requests
 
-SEARCH_RANGE = timedelta(days=7)
 COUNTRY_CODE = "CA"
-TXT_PATH = Path.home().joinpath("wca.txt")
+COMPS_FILE = Path.home().joinpath("wca.txt")
+COMP_IDS_FILE = Path.home().joinpath("wca-comp-ids.txt")
 FIRST_LINE = "Upcoming WCA events in Canada (in ~/wca.txt)"
 
 
@@ -47,22 +47,19 @@ class Competition:
             f"""
             {self.name}
                 City:         {self.city}
-                Date:         {self.start_date} to {self.end_date}
-                Registration: {self.registration_open} to {self.registration_close}
+                Date:         {self.start_date} to {self.end_date} (registration {self.registration_open} to {self.registration_close})
                 Website:      {self.website}
             """
         ).strip()
 
 
-def get_recently_announced_competitions() -> list[Competition]:
+def get_canadian_comps() -> list[Competition]:
     """
     Fetch the list of recently-announced competitions from the WCA API.
     """
-    announced_after = datetime.now() - SEARCH_RANGE
     url = (
         "https://www.worldcubeassociation.org/api/v0/competitions"
         + f"?country_iso2={COUNTRY_CODE}"
-        + f"&announced_after={announced_after.strftime('%Y-%m-%d')}"
     )
     response = requests.get(url, timeout=60)
     if response.status_code // 100 != 2:
@@ -84,31 +81,53 @@ def get_recently_announced_competitions() -> list[Competition]:
     ]
 
 
-def store_comps(comps: list[Competition]) -> None:
+def store_comp_details(comps: list[Competition]) -> None:
     """Update the text file with the new competitions."""
     comps = sorted(comps, key=lambda c: c.announced_at)
     try:
-        old_text = TXT_PATH.read_text(encoding="utf-8")
+        old_text = COMPS_FILE.read_text(encoding="utf-8")
     except FileNotFoundError:
         old_text = ""
     lines = old_text.split("\n")
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     for i, line in enumerate(lines):
         if line.startswith(FIRST_LINE):
-            lines[i] = f"{FIRST_LINE} as of {now}"
+            lines[i] = f"{FIRST_LINE} as of {now}:"
     if not any(ln.startswith(FIRST_LINE) for ln in lines):
         lines = [f"{FIRST_LINE} as of {now}"] + lines
     msg = "\n".join(lines).strip()
     msg += "\n\n"
     msg += "\n\n".join([str(c) for c in comps])
     msg += "\n"
-    TXT_PATH.write_text(msg, encoding="utf-8")
+    COMPS_FILE.write_text(msg, encoding="utf-8")
+
+
+def get_old_comp_ids() -> list[str]:
+    """Get the list of previously-seen competition IDs."""
+    try:
+        txt = COMP_IDS_FILE.read_text(encoding="utf-8")
+    except FileNotFoundError:
+        return []
+    return [x.strip() for x in txt.split("\n") if x.strip()]
+
+
+def store_comp_ids(comp_ids: list[str]) -> None:
+    """Update the file storing the list of previously-seen competition IDs."""
+    sorted_unique_ids = sorted(set(comp_ids))
+    COMP_IDS_FILE.write_text("\n".join(sorted_unique_ids), encoding="utf-8")
 
 
 def main() -> None:
     """Run the script."""
-    new_comps = get_recently_announced_competitions()
-    store_comps(new_comps)
+    old_comp_ids = get_old_comp_ids()
+    new_comps = get_canadian_comps()
+    new_comps = [
+        c
+        for c in new_comps
+        if not c.is_registration_closed() and c.cid not in old_comp_ids
+    ]
+    store_comp_details(new_comps)
+    store_comp_ids(old_comp_ids + [c.cid for c in new_comps])
 
 
 if __name__ == "__main__":
